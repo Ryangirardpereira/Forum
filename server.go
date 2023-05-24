8108bb16b83056f459793bb8b58ab17290301956
fpierrea <pierrealexis.favril@ynov.com>
2023-05-24 09:11:30 +0200

    création du server en go

--- server.go
+++ server.go
@@ -6,8 +6,13 @@ import (
 	"net/http"
 )
 
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
 func main() {
 	fmt.Println("http://localhost:8080")
+	http.HandleFunc("/", homeHandler)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
