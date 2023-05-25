294fff4a00f54b3478149f956caec81c94121e25
fpierrea <pierrealexis.favril@ynov.com>
2023-05-25 11:13:56 +0200

    mise en place de la page html

--- server.go
+++ server.go
@@ -2,18 +2,30 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
+type Data struct {
+	name string
+}
 
+var UserData Data
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("./static/html/Home.html")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	t.Execute(w, UserData)
 }
 
 func main() {
 	fmt.Println("http://localhost:8080")
-	http.HandleFunc("/", homeHandler)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	http.HandleFunc("/", homeHandler)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
