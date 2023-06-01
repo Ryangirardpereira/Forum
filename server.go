0a81882bfe9f8dfb9194e6b6fbbe972ef3e98962
fpierrea <pierrealexis.favril@ynov.com>
2023-06-01 10:25:29 +0200

    tentative requete

--- server.go
+++ server.go
@@ -11,7 +11,12 @@ import (
 )
 
 type Data struct {
-	name string
+	id_user    int
+	password   string
+	email      string
+	username   string
+	first_name string
+	last_name  string
 }
 
 var UserData Data
@@ -31,7 +36,18 @@ func Co_BDD() {
 		fmt.Println("Erreur lors de la connexion à la base de données:", err)
 		return
 	}
+
 	defer db.Close()
+
+	query := "INSERT INTO user (id_user, password, email, username, first_name, last_name) VALUES (1,'test','test@gmail.com','test','mon_prenom_est','mon_nom_est')"
+
+	rows, err := db.Query(query)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer rows.Close()
 
 	err = db.Ping()
 	if err != nil {
